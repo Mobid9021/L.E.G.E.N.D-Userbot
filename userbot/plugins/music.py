bbdf31d7bff64bbc9c27495b638d443a829a85e9
RamshourieshR <68327188+RamshourieshR@users.noreply.github.com>
2020-07-17 12:48:01 +0530

    Update music.py

--- userbot/plugins/music.py
+++ userbot/plugins/music.py
@@ -1,119 +1,84 @@
-# Originally from Bothub
-# Port to UserBot by @heyworld
-#Copyright (C) 2020 azrim.
+import html
+import time
+import datetime
+from telegram.ext import CommandHandler, run_async, Filters
+import requests, logging
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS
+from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
+from tg_bot.modules.helper_funcs.chat_status import user_admin
 
-from telethon import events
-import asyncio
-#from userbot.utils import admin_cmd
-from userbot.events import register 
-from userbot import bot, CMD_HELP
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-import os
-try:
- import subprocess
-except:
- os.system("pip install instantmusic")
- 
+count = 0
+@run_async
+def music(bot: Bot, update: Update, args):
+	message = update.effective_message
+	global count
 
-
-os.system("rm -rf *.mp3")
-
-
-def bruh(name):
+	chatId = update.message.chat_id
     
-    os.system("instantmusic -q -s "+name)
-    
+	video_id = ''.join(args)
+
+	if video_id.find('youtu.be') != -1:
+		index = video_id.rfind('/') + 1
+		video_id = video_id[index:][:11]
+		message.reply_text("Please wait...\ndownloading audio.")
+
+	elif video_id.find('youtube') != -1:
+		index = video_id.rfind('?v=') + 3
+		video_id = video_id[index:][:11]
+		message.reply_text("Please wait...\downloading audio.")
+
+	elif not video_id.find('youtube') != -1:
+		message.reply_text("Please provide me youtube link")
+
+	elif not video_id.find('youtu.be') != -1:
+		message.reply_text("Please provide me youtube link")
+		
+
+        
 
 
-@register(outgoing=True, pattern="^.spd(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    link = event.pattern_match.group(1)
-    chat = "@SpotifyMusicDownloaderBot"
-    await event.edit("```Getting Your Music```")
-    async with bot.conversation(chat) as conv:
-          await asyncio.sleep(2)
-          await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
-          try:
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
-              await bot.send_message(chat, link)
-              respond = await response
-          except YouBlockedUserError:
-              await event.reply("```Please unblock @SpotifyMusicDownloaderBot and try again```")
-              return
-          await event.delete()
-          await bot.forward_messages(event.chat_id, respond.message)
 
-@register(outgoing=True, pattern="^.netease(?: |$)(.*)")
-async def WooMai(netase):
-    if netase.fwd_from:
-        return
-    song = netase.pattern_match.group(1)
-    chat = "@WooMaiBot"
-    link = f"/netease {song}"
-    await netase.edit("```Getting Your Music```")
-    async with bot.conversation(chat) as conv:
-          await asyncio.sleep(2)
-          await netase.edit("`Downloading...Please wait`")
-          try:
-              msg = await conv.send_message(link)
-              response = await conv.get_response()
-              respond = await conv.get_response()
-              """ - don't spam notif - """
-              await bot.send_read_acknowledge(conv.chat_id)
-          except YouBlockedUserError:
-              await netase.reply("```Please unblock @WooMaiBot and try again```")
-              return
-          await netase.edit("`Sending Your Music...`")
-          await asyncio.sleep(3)
-          await bot.send_file(netase.chat_id, respond)
-    await netase.client.delete_messages(conv.chat_id,
-                                       [msg.id, response.id, respond.id])
-    await netase.delete()
+	r = requests.get(f'https://api.pointmp3.com/dl/{video_id}?format=mp3')
+	
+
+	json1_response = r.json()
+
+	if not json1_response['error']:
+		
+
+		redirect_link = json1_response['url']
+
+		r = requests.get(redirect_link)
+		
+
+		json2_response = r.json()
+
+		if not json2_response['error']:
+			payload = json2_response['payload']
+
+			info = '*{0}* \nUploaded by @AnonymousD3061'.format(payload['fulltitle'])
+
+			try:
+				
+				bot.send_audio(chat_id=chatId, audio=json2_response['url'] ,parse_mode='Markdown',text="meanya", caption=info)
+				count += 1
+				print("\033[1m\033[96m" + "Download count: " + str(count) + "\033[0m")
+			except:
+				bot.send_message(chat_id=chatId, text='Something went wrong with the download..!\nPlease Report there @AnonymousD3061')
 
 
-@register(outgoing=True, pattern="^.dzd(?: |$)(.*)")
-async def DeezLoader(Deezlod):
-    if Deezlod.fwd_from:
-        return
-    d_link = Deezlod.pattern_match.group(1)
-    if ".com" not in d_link:
-        await Deezlod.edit("` I need a link to download something pro.`**(._.)**")
-    else:
-        await Deezlod.edit("**Initiating Download!**")
-    chat = "@DeezLoadBot"
-    async with bot.conversation(chat) as conv:
-          try:
-              msg_start = await conv.send_message("/start")
-              response = await conv.get_response()
-              r = await conv.get_response()
-              msg = await conv.send_message(d_link)
-              details = await conv.get_response()
-              song = await conv.get_response()
-              """ - don't spam notif - """
-              await bot.send_read_acknowledge(conv.chat_id)
-          except YouBlockedUserError:
-              await Deezlod.edit("**Error:** `unblock` @DeezLoadBot `and retry!`")
-              return
-          await bot.send_file(Deezlod.chat_id, song, caption=details.text)
-          await Deezlod.client.delete_messages(conv.chat_id,
-                                             [msg_start.id, response.id, r.id, msg.id, details.id, song.id])
-          await Deezlod.delete()          
-    
-CMD_HELP.update({
-        "music":
-        ".spd`<Artist - Song Title>\
-            \nUsage:For searching songs from Spotify.\
-            \n\n`.netease` <Artist - Song Title>\
-            \nUsage:Download music with @WooMaiBot\
-            \n\n`.dzd` <Spotify/Deezer Link>\
-            \nUsage:Download music from Spotify or Deezer.\
-            \n\n`.deezload` <spotify/deezer link> <Format>\
-            \nUsage: Download music from deezer.\
-            \n\n Well deezer is not available in India so create an deezer account using vpn. Set DEEZER_ARL_TOKEN in vars to make this work.\
-            \n\n *Format= `FLAC`, `MP3_320`, `MP3_256`, `MP3_128`.\
-            \n\n\n Guide:Video guide of arl token: [here](https://www.youtube.com/watch?v=O6PRT47_yds&feature=youtu.be) or Read [This](https://notabug.org/RemixDevs/DeezloaderRemix/wiki/Login+via+userToken)."
-})
+__help__ = """ Youtube audio Downloader
+ - /music <Youtube link> : download audio file from youtube link.
+
+"""
+__mod_name__ = "MP3 DOWNLOADER" 
+
+music_handler = CommandHandler('music', music, pass_args=True)
+dispatcher.add_handler(music_handler)
+
+
+
 
 
