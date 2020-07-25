bf7cd57d9120b81fc2da625c0d8646e82fc88e8f
J.A.R.V.I.S <68327188+jarvis210904@users.noreply.github.com>
2020-07-25 21:42:31 +0530

    Update telesetup.py

--- telesetup.py
+++ telesetup.py
@@ -12,7 +12,7 @@ while True:
    with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
       print("")
       session = client.session.save()
-      client.send_message("me", f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` And Visit @FridayOT For Any Help !")
+      client.send_message("me", f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` And Visit @jarvissupportofficial For Any Help !")
       print("You telegramString session successfully stored in your telegram, please check your Telegram Saved Messages ")
       print("Store it safe !!")
       print("Thanks For Choosing JARVIS Service. Hope that You will Have a Goodtime With Us!....)
