45438b4ad909c6e3de0f4ae70af65b347d6885bf
J.A.R.V.I.S <68327188+jarvis210904@users.noreply.github.com>
2020-07-26 07:01:05 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,26 +1,33 @@
-"""Check if userbot alive."""
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+#IMG CREDITS: @WhySooSerious
 import asyncio
 from telethon import events
 from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/Anonymous-07-25"
-pm_caption = "`💠JARVIS IS💠:` **ONLINE**\n\n"
+PM_IMG = "https://telegra.ph/file/72b8334d6855f6a9c9839.jpg"
+pm_caption = "`💠FRIDAY IS💠:` **ONLINE**\n\n"
+pm_caption += "**📥Support Channel📥** : [🔰Join Support Channel🔰](https://t.me/jarvissupportofficial)\n\n"
+pm_caption += "**📥Support Group📥** : [🔰Join Support Group🔰](https://t.me/joinchat/R5-ZBEdh9Uzix53RWtmHiA)\n\n"
+
 pm_caption += "**🔖SYSTEM STATUS🔖**\n\n"
 pm_caption += "`⚖️TELETHON VERSION⚖️:` **6.0.9**\n`Python:` **3.7.4**\n\n"
 pm_caption += "`🎢DATABASE STATUS🎢:` **Functional**\n\n"
 pm_caption += "**🧮Current Branch🧮** : `master`\n\n"
-pm_caption += "**📬JARVIS OS📬** : `3.14`\n\n"
+pm_caption += "**📬Friday OS📬** : `3.14`\n\n"
 pm_caption += "**💡Current Sat💡** : `StarkGangSat-2.25`\n\n"
 pm_caption += f"*💜*My Boss💜** : {DEFAULTUSER} \n\n"
 pm_caption += "**💥Heroku Database💥** : `AWS - Working Properly`\n\n"
-pm_caption += "**🚫License🚫** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
-pm_caption += "⛔️Copyright : By⛔️ [StarkGang@Github](GitHub.com/StarkGang)\n\n\n\n"
+pm_caption += "**🚫License🚫** : [MIT Licence](https://github.com/jarvis210904/J.A.R.V.I.S-Userbot/blob/master/LICENSE)\n\n"
+pm_caption += "⛔️Copyright : By⛔️ [JARVIS@Github](GitHub.com/jarvis210409)\n\n\n\n"
+pm_caption += " [Deploy JARVIS]"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     chat = await alive.get_chat()
-    """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption
     await alive.delete()
+    """ For .alive command, check if the bot is running.  """
+    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+    await alive.delete() 
+    
