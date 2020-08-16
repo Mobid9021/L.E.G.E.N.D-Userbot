426f284663b91395843070f1783eb6a8caa85bf2
L.E.G.E.N.D <68838750+KeshavTech246@users.noreply.github.com>
2020-08-16 23:45:00 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -4,16 +4,21 @@ from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/e5c4be66f4c034560b736.jpg"
-pm_caption = "**L.E.G.E.N.D is online**\n"
+PM_IMG = "https://telegra.ph/file/3dcab7a6a6309042ffb0c.jpg"
+pm_caption = "**ᴊᴀʀᴠɪꜱ ɪꜱ ᴏɴʟɪɴᴇ**\n"
 
-pm_caption += "\n**My Legendary Boss**: {DEFAULTUSER}\n"
+pm_caption += f"**My Legendary Boss**: {DEFAULTUSER}\n"
 
-pm_caption += "\n**Telethon Version**: 4.8\n"
+pm_caption += "Telethon Version: 4.8\n"
 
-pm_caption += "\n**Python Version**: 3.8.5\n"
+pm_caption += "Python Version: 3.8.5\n"
 
-pm_caption += "\n**I am a cool userbot with many functions managed by [this legendary person.](https://t.me/The_Anonymous_Legend)**\n"
+pm_caption += "Bot Version: 7.0.9\n"
+
+pm_caption += "License: [AGPL-3.0 Licence](https://github.com/KeshavTech246/L.E.G.E.N.D-userbot/blob/master/LICENSE/)\n"
+
+pm_caption += "Copyright by: L.E.G.E.N.D\n"
+
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
