d98aadebbfb9eb195821992d50ee37d7c1384afb
L.E.G.E.N.D <68838750+KeshavTech246@users.noreply.github.com>
2020-08-16 13:05:45 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,13 +7,13 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/e5c4be66f4c034560b736.jpg"
 pm_caption = "**L.E.G.E.N.D is online**\n"
 
-pm_caption += "\n**My Legendary Boss**            : @The_Anonymous_Legend\n"
+pm_caption += "\n**My Legendary Boss**: @The_Anonymous_Legend\n"
 
-pm_caption += "\n**Telethon Version**            : 4.8\n"
+pm_caption += "\n**Telethon Version**: 4.8\n"
 
-pm_caption += "\n**Python Version**            : 3.8.5\n"
+pm_caption += "\n**Python Version**: 3.8.5\n"
 
-pm_caption += "\nI am a cool userbot with many functions managed by [this legendary person.](https://t.me/The_Anonymous_Legend)\n"
+pm_caption += "\nI am a cool userbot with many functions managed by [**this legendary person**.](https://t.me/The_Anonymous_Legend)\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
