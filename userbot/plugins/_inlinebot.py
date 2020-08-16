deb4d86dc5005bc0eca591b5e0305ac1636843c2
KeshavTech246 <68838750+KeshavTech246@users.noreply.github.com>
2020-08-16 10:00:31 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -57,7 +57,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own THE L.E.G.E.N.D Userbot, don't use Mine\n ib @The_Anonymous_Legend for learning how to get userbot!"
+            reply_pop_up_alert = "Get your own L.E.G.E.N.D Userbot, don't use Mine\n ib @The_Anonymous_Legend for learning how to get userbot!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"us_plugin_(.*)")
