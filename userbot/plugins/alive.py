f9002c1cff65eaf951ade1c191cbd78003fdc1ae
L.E.G.E.N.D <68838750+KeshavTech246@users.noreply.github.com>
2020-08-16 23:53:35 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,17 +7,15 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/e5c4be66f4c034560b736.jpg"
 pm_caption = "**L.E.G.E.N.D is ONLINE**\n"
 
-pm_caption += f"**My Legendary Boss**: {DEFAULTUSER}\n"
+pm_caption += f"\n**My Legendary Boss**: {DEFAULTUSER}\n"
 
-pm_caption += "Telethon Version: 4.8\n"
+pm_caption += "\nTelethon Version: 4.8\n"
 
-pm_caption += "Python Version: 3.8.5\n"
+pm_caption += "\nPython Version: 3.8.5\n"
 
-pm_caption += "Bot Version: 7.0.9\n"
+pm_caption += "\nLicense: [AGPL-3.0 Licence](https://github.com/KeshavTech246/L.E.G.E.N.D-userbot/blob/master/LICENSE/)\n"
 
-pm_caption += "License: [AGPL-3.0 Licence](https://github.com/KeshavTech246/L.E.G.E.N.D-userbot/blob/master/LICENSE/)\n"
-
-pm_caption += "Copyright by: L.E.G.E.N.D\n"
+pm_caption += "\nCopyright by: [L.E.G.E.N.D](https://github.com/KeshavTech246)\n"
 
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
