da878bdc46f7da8214f4c5c0709e7d4faef92660
L.E.G.E.N.D <68838750+KeshavTech246@users.noreply.github.com>
2020-08-16 12:58:39 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,15 +5,15 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/e5c4be66f4c034560b736.jpg"
-pm_caption = "**THE L.E.G.E.N.D is online**\n"
+pm_caption = "**L.E.G.E.N.D is online**\n"
 
-pm_caption += "\n**My Legendary Boss**            : {DEFAULTUSER}\n"
+pm_caption += "\n**My Legendary Boss**            : @The_Anonymous_Legend\n"
 
-pm_caption += "\nTelethon Version            : 4.8\n"
+pm_caption += "\n**Telethon Version**            : 4.8\n"
 
-pm_caption += "\nPython Version            : 3.8.5\n"
+pm_caption += "\n**Python Version**            : 3.8.5\n"
 
-pm_caption += "I am a cool userbot with many functions managed by [this legendary person.](https://t.me/The_Anonymous_Legend) Contact him for any help or support\n"
+pm_caption += "\nI am a cool userbot with many functions managed by [this legendary person.](https://t.me/The_Anonymous_Legend) Contact him for any help or support\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
