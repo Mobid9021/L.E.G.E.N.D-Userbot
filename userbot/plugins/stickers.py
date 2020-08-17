0428504640684918798e3e058a8191a73eca90bb
L.E.G.E.N.D <68838750+KeshavTech246@users.noreply.github.com>
2020-08-17 10:31:17 +0530

    Fixed Stickers.py 😎
    changed the kang emoji

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -77,7 +77,7 @@ async def kang(args):
     if photo:
         splat = args.text.split()
         if not emojibypass:
-            emoji = "😂"
+            emoji = "😎"
         pack = 1
         if len(splat) == 3:
             pack = splat[2]  # User sent both
